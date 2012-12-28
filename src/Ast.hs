2a416f81831ab3076359df5d3b4af66fbc39e8c8
Andrea Corradi <andreac@unstable.it>
2012-12-28 22:28:19 +0100

    Add Negative, Not, Cast data constructor

--- src/Ast.hs
+++ src/Ast.hs
@@ -1,10 +1,14 @@
 module Ast where
 
 type Operation = String
+type TypeName = String
 data Expression = I Int | B Bool | S String | Var String | Empty
                   | Additive Operation Expression Expression
                   | Multiplicative Operation Expression Expression
                   | Relational Operation Expression Expression
                   | Equality Operation Expression Expression
                   | Boolean Operation Expression Expression
+                  | Negative Expression
+                  | Not Expression
+                  | Cast TypeName Expression
                   deriving(Show,Eq)
