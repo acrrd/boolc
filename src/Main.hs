30e1337afe69b282734313e4727587a6e8511eed
Andrea Corradi <andreac@unstable.it>
2013-01-20 19:13:10 +0100

    Parse a file given as command line parameter

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,26 @@
 module Main(main) where
 
-main :: IO ()
-main = return ()
+import Parser
 
+import System.Environment
+import System.Exit (exitSuccess)
+import System.IO
+import Control.Monad
+
+printUsage :: IO ()
+printUsage = do pname <- getProgName 
+                putStrLn ("Usage: " ++ pname ++ " <file>")
+
+parseFile :: String -> String -> String
+parseFile fn fc = case (runBoolParser fn) fc of
+                    Left e -> show e
+                    Right v -> show v 
+
+main :: IO ()
+main = do args <- getArgs
+          let nargs = length args
+          when (nargs /= 1) (printUsage >> exitSuccess)
+          let filename = args !! 0
+          ret <- liftM (parseFile filename) $ readFile filename
+          putStrLn ret
+          return ()
