bcbb804c09a9fc7f1eae92f46aa49937f0d3e230
Andrea Corradi <andreac@unstable.it>
2013-05-07 16:03:27 +0200

    Parse file, typecheck the ast and call the code generator

--- src/Main.hs
+++ src/Main.hs
@@ -1,29 +1,38 @@
 module Main(main) where
 
-import Parser
+import Ast
 import AstUtils
+import Parser
 import Typesystem
 import ToLLVM
+
+import Text.ParserCombinators.Parsec (ParseError)
+
+import Control.Monad
 
 import System.Environment
 import System.Exit (exitSuccess)
 import System.IO
-import Control.Monad
+import System.FilePath
 
 printUsage :: IO ()
 printUsage = do pname <- getProgName 
                 putStrLn ("Usage: " ++ pname ++ " <file>")
 
-parseFile :: String -> String -> String
-parseFile fn fc = case (runBoolParser fn) fc of
-                    Left e -> show e
-                    Right v -> show v 
+parseFile :: String -> IO (Either ParseError ProgramSP)
+parseFile fn = liftM (runBoolParser fn) $ readFile fn
 
 main :: IO ()
 main = do args <- getArgs
           let nargs = length args
           when (nargs /= 1) (printUsage >> exitSuccess)
-          let filename = args !! 0
-          ret <- liftM (parseFile filename) $ readFile filename
-          putStrLn ret
-          return ()
+          let filepath = args !! 0
+          let fileoutput = replaceExtension (snd $ splitFileName filepath) ".bc"
+          parseresult <- parseFile filepath
+          case parseresult of
+            Left perr -> putStrLn $ show perr
+            Right ast -> do let ast' = desugar ast
+                            case typecheck ast' of
+                              Left terr -> putStrLn $ show terr
+                              Right (astt,cte) -> genLLVMCode astt cte fileoutput
+
